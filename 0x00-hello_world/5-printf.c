0b96ef4d6d1e05281eeec06df9b05ed53e790edb
David <davidkwaku98@gmail.com>
2023-02-16 12:02:01 -0800

    0x00-hello_world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-printf("with proper grammar, but the outcome is a piece of art,\n");
+#include <stdio.h>
+
+/**
+ * main - The entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
